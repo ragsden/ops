8dbbf38a18f878fe76ff865c3845aab2eda81e8d
sharathm <codemod@gmail.com>
2014-01-07 12:19:38 +0530

    moving the file

--- tests/test-container-page.js
+++
@@ -1,5 +0,0 @@
-describe('container page tests',function() {
-	it('gets account information for a given Id');
-	it('gets container information for a given Id');
-	it('shows error when valid github Id is not found');
-});
