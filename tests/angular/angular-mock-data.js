e6aefcb0b193a7f9fff6aa91cd49e69bfefd7d81
vamshis <vamshi@shippable.com>
2014-01-13 15:33:21 +0530

    update global angular test-data, Fixes #141

--- tests/angular/angular-mock-data.js
+++ tests/angular/angular-mock-data.js
@@ -24,6 +24,29 @@ var config = {
               { 'id' : '127f1f77bcf86cd799439012','name' : 'Test-2'}
             ],
             'createNodePOST' : { 'type' : '707f4f77bcf86cd799439012' },
-            'createNodePOSTNegative' : { 'type' : '707f4f77bcf86cd799439012' }
+            'createNodePOSTNegative' : { 'type' : '707f4f77bcf86cd799439012' },
+      
+          //subscriptionsPage test-data
+          'subscriptions': 
+              [{
+                  id: '123f1f77bcf86cd799439011',
+                  name: 'Free Plan',
+                  plan: '0000000000000000000000000000000X',
+                  projects: ['testProject'],
+                  containers: ['507f1f77bcf86cd799439011'],
+                  owners: ['testOwner'],
+                  created: '2013-Dec-01 13:54 PM (PST)',
+                  updated: '2014-Jan-03 22:54 PM (PST)'
+              }],
+
+          'plan': 
+              {
+                id: '0000000000000000000000000000000X',
+                name: 'Free',
+                containerQuota: 1,
+                privateProjectsQuota: 1,
+                storageGigaBytesQuota: 1
+              }
+      
       }
 
