98bcaa59671edb905a02226b9459efb6321b706e
vamshis <vamshi@shippable.com>
2014-01-17 14:55:03 +0530

    delete subscription function added, Fixes #183

--- public/js/app/services/subscriptionsService.js
+++ public/js/app/services/subscriptionsService.js
@@ -12,7 +12,22 @@ angSpa.factory('subscriptionsService',function($http){
           done(message, null);
         });
      };
+
+
+     subscriptionsService.prototype.deleteSubscriptionBySubId = function(subId, done){
+        var subsUrl = middlewareUrl + "/subscriptions/" + subId;
+        $http({ method: 'DELETE', url: subsUrl}).
+        success(function(data, status, headers, config) {
+          done(null, status);
+        }).
+        error(function(data, status, headers, config) {
+          var message = "Error in deleting subscription using Subscription Id";
+          done(message, null);
+        });
+     };
+
    };
+
    return new subscriptionsService();
 
 });
