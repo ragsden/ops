3253b2574bad8fd1bcad78e303e5eaad11071089
swati730 <shippable@shippable-Vostro-2520.(none)>
2014-01-21 12:06:28 +0530

    Fixes #215

--- public/js/app/controllers/projectsController.js
+++ public/js/app/controllers/projectsController.js
@@ -17,9 +17,11 @@ var ProjectsController = function($scope,$routeParams,$location,ProjectsService)
               roles:[""]
            }]         
          }],
-         err:"",        
+         err:"",
+         status:"",        
       };
   
+    $scope.init = function(){
     ProjectsService.getProjectsBySubscriptionId($routeParams.subscriptionId,function(err,data){
       if(err) 
       {
@@ -35,7 +37,23 @@ var ProjectsController = function($scope,$routeParams,$location,ProjectsService)
       }        
             
       }
-   });
+    });
+   }
+
+  $scope.deleteProject = function(projectId){
+    console.log(projectId);
+    ProjectsService.deleteProjectById(projectId, function(status, data){
+      if(status === 200){
+        $scope.projectsModel.status = 'The project has been deleted. ' + data 
+        $scope.init();
+      }
+      else
+      {
+        $scope.projectsModel.err = 'There was an error while deleting this project :' + data;
+      }
+    });
+  };
+  $scope.init();
   };
 ProjectsController.$inject = ["$scope","$routeParams","$location","ProjectsService"];
 angSpa.controller("projectsController",ProjectsController);
