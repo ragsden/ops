854de384c03528fa811d05245dbf7ba7ddc74e5f
sharathm <codemod@gmail.com>
2014-01-21 11:59:56 +0530

    controller and services for builds page. Fixes #208

--- public/js/app/controllers/buildsController.js
+++ public/js/app/controllers/buildsController.js
@@ -1,7 +1,33 @@
 
 'use strict';
-angular.module('angSpa').controller('buildsController',['$scope','$routeParams',
-	function($scope,$routeParams) 
+angular.module('angSpa').controller('buildsController',['$scope','$routeParams','BuildsService',
+	function($scope,$routeParams,buildsService) 
 	{
-
+		$scope.builds = [];
+		$scope.errors = [];
+			buildsService.getBuildsByProjectId($routeParams.projectId,function(err,data) {
+				if(err) {
+					$scope.errors.push('Error getting builds information..' + err + ',' + data);
+				}
+				else {
+					if(data.length === 0)
+					{
+						$scope.errors.push('There are no builds in this project');
+					} 
+					else {
+						for(var i=0;i<data.length;i++) {
+							$scope.builds.push({
+								buildNumber : data[i].buildNumber,
+								phase : data[i].phase,
+								status : data[i].status,
+								duration : data[i].duration,
+								commitSha : data[i].commitSha,
+								commitUrl : data[i].commitUrl,
+								lastAuthorEmail: data[i].lastAuthorEmail,
+								triggeredByAccount : data[i].triggeredByAccount
+							});
+						}
+					}
+				}
+			});
 	}]);
---
+++ tests/angular/test-buildsService.js
@@ -0,0 +1,36 @@
+describe('Builds Service',function() {
+	var httpBackend;
+	var buildService;
+	beforeEach(function() {
+		module('angSpa');
+		inject(function($httpBackend,BuildsService) {
+			httpBackend = $httpBackend;
+			buildService = BuildsService;
+		});
+
+		
+	});
+	describe('Node Service',function() {
+		
+		it('gets the builds for a project',function() {
+			var result;
+			httpBackend.when('GET',config.MW_URL+'/projects/'+testData.testProjectId+'/builds')
+				.respond(200,testData.testProjectData);
+
+			buildService.getBuildsByProjectId(testData.testProjectId,function(err,data) {
+				result = data;
+			});
+
+				
+			httpBackend.flush();
+			expect(result[0].buildNumber).toBe(1);
+			expect(result[0].phase).toBe('Queued');
+		});
+
+	});
+	
+	afterEach(function() {
+		httpBackend.verifyNoOutstandingExpectation();
+     	httpBackend.verifyNoOutstandingRequest();
+	});
+});
