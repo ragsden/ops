04746d1b3a0d327658fe898c399ce1778954f317
vamshis <vamshi@shippable.com>
2014-01-21 18:22:58 +0530

    added tests on getting info on subs page, Fixes #225

--- tests/angular/test-subscriptionsController.js
+++ tests/angular/test-subscriptionsController.js
@@ -7,24 +7,28 @@ describe('Testing subscriptionsController',function() {
 	var httpBackend;
 	beforeEach(function() {
 		module('angSpa');
-		inject(function($rootScope, $httpBackend, $routeParams, $controller, subscriptionsService, plansService) {
+		inject(function($rootScope, $httpBackend, $routeParams, $controller, subscriptionsService, plansService, AccountsService) {
 			ctrlScope = $rootScope.$new();
             subsServ = subscriptionsService;
             plansServ = plansService;
+            accountsServ = AccountsService;
 			routeParams = $routeParams;
 			httpBackend = $httpBackend;
 
+            spyOn(accountsServ, 'getAccountById').andCallThrough();
             spyOn(subsServ, 'getSubscriptionsByAccountId').andCallThrough();
             spyOn(plansServ, 'getPlanByPlanId').andCallThrough();
             spyOn(subsServ, 'deleteSubscriptionBySubId').andCallThrough();
 
-        httpBackend.expect('GET', config.MW_URL + '/accounts/' + testData.accountIdGETParam + '/subscriptions')
+            httpBackend.expect('GET', config.MW_URL + "/accounts/" + testData.accountIdGETParam)
+            .respond(200, testData.accountGET);
+            httpBackend.expect('GET', config.MW_URL + '/accounts/' + testData.accountIdGETParam + '/subscriptions')
 		    .respond(200, testData.subscriptionsGET);
-		httpBackend.expect('GET', config.MW_URL + '/plans/' + testData.subscriptionsGET[0].plan)
+		    httpBackend.expect('GET', config.MW_URL + '/plans/' + testData.subscriptionsGET[0].plan)
 			.respond(200, testData.planGET);
         
         
-        routeParams.accountId = testData.accountIdGETParam;
+            routeParams.accountId = testData.accountIdGETParam;
             routeParams.planId = testData.planIdGETParam;
 			
 			ctrl = $controller('subscriptionsController',
@@ -33,6 +37,7 @@ describe('Testing subscriptionsController',function() {
 					$routeParams : routeParams,
                     subscriptionsService : subsServ,
                     plansService : plansServ,
+                    AccountsService : accountsServ
 			     });
 			
 			});
@@ -41,8 +46,11 @@ describe('Testing subscriptionsController',function() {
 	it('should call getSubscriptionsByAccountId, getPlanByPlanId when the controller is created', function() {
         
         httpBackend.flush();
+
+        expect(accountsServ.getAccountById).toHaveBeenCalled();
         expect(subsServ.getSubscriptionsByAccountId).toHaveBeenCalled();
         expect(plansServ.getPlanByPlanId).toHaveBeenCalled();
+        expect(ctrlScope.subscriptionsModel.accountInfo.identities[0].userName).toBe('testUser');
         expect(ctrlScope.subscriptionsModel.subscriptions.length).toBe(1);
         expect(ctrlScope.subscriptionsModel.subscriptions[0].id).toBe('123f1f77bcf86cd799439011');
         expect(ctrlScope.subscriptionsModel.subscriptions[0].plan).toBe('0000000000000000000000000000000X');
@@ -59,6 +67,7 @@ describe('Testing subscriptionsController',function() {
         httpBackend.flush();
 
         //now the two calls are expected.
+        expect(accountsServ.getAccountById).toHaveBeenCalled();
         expect(subsServ.getSubscriptionsByAccountId).toHaveBeenCalled();
         expect(plansServ.getPlanByPlanId).toHaveBeenCalled();
 
@@ -68,6 +77,8 @@ describe('Testing subscriptionsController',function() {
         //Since init() is being called again (get subs, plan are executed again)
         httpBackend.expect('DELETE', config.MW_URL + '/subscriptions/'+ testData.subIdDELParam)
         .respond(200, 'OK');
+		httpBackend.expect('GET', config.MW_URL + '/accounts/' + testData.accountIdGETParam)
+		.respond(200, testData.accountGET);
         httpBackend.expect('GET', config.MW_URL + '/accounts/' + testData.accountIdGETParam + '/subscriptions')
 	    .respond(200, testData.subscriptionsGET);
 		httpBackend.expect('GET', config.MW_URL + '/plans/' + testData.subscriptionsGET[0].plan)
@@ -80,6 +91,7 @@ describe('Testing subscriptionsController',function() {
         httpBackend.flush();
 
         //expect them in this order
+        expect(accountsServ.getAccountById).toHaveBeenCalled();
         expect(subsServ.deleteSubscriptionBySubId).toHaveBeenCalled();
         expect(subsServ.getSubscriptionsByAccountId).toHaveBeenCalled();
         expect(plansServ.getPlanByPlanId).toHaveBeenCalled();
