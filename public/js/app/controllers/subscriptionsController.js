473f3bb775e1c465f9fc794d5ceb8455bc891544
swati730 <shippable@shippable-Vostro-2520.(none)>
2014-01-31 17:32:43 +0530

    Fixes #340 , updated mock-data

--- public/js/app/controllers/subscriptionsController.js
+++ public/js/app/controllers/subscriptionsController.js
@@ -59,10 +59,9 @@ var SubscriptionsController = function($scope, $location, subscriptionsService, 
           var j = i;
           plansService.getPlanByPlanId(subsData[j].plan, function(errP, planData){
             if(!errP){
-              var percent_storageBytesUsed = (subsData[j].storageBytesUsed/(planData.storageGigaBytesQuota * 1073741824))*100;
+              var percent_storageBytesUsed = ((subsData[j].storageBytesUsed/(planData.storageGigaBytesQuota * 1073741824))*100).toFixed(2);
               var percent_privateProjectsUsed = (subsData[j].privateProjectsCount/planData.privateProjectsQuota)*100;
-              var arr = ['1'];
-              var nodesUsed = arr.length; // TODO change it to subsData[j].nodes.length
+              var nodesUsed = subsData[j].nodes.length;
               var percent_nodesUsed = (nodesUsed/planData.nodesQuota)*100;
               var subscriptionData = new subscriptionDataObject(subsData[j].id,
                                                                 subsData[j].name,
