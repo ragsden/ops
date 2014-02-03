f35b5f3106a6b9828f7b02865396d0cc530472f6
vamshis <vamshi@shippable.com>
2014-02-03 19:48:02 +0530

    using data from services, Fixes #359

--- public/js/app/controllers/projectPermissionsModalCtrl.js
+++ public/js/app/controllers/projectPermissionsModalCtrl.js
@@ -1,13 +1,38 @@
 (function(){
-   var ProjectPermissionsModalController = function($scope, $modalInstance){
+   var ProjectPermissionsModalController = function($scope, $modalInstance, projectsService, AccountsService){
+
+     
 
       $scope.cancel = function(){
         $modalInstance.dismiss('cancel');
       };
 
+      $scope.refresh = function(){
+        $scope.collaborators = [];
+        projectsService.getProjectByProjectId(projectId, function(projDataErr, projectData){
+          if(!projDataErr){
+          var i, j, l = projectData.permissions.length;
+          for (i=0; i < l; i++){
+            j = i;
+            //get accounts using acc ids
+            AccountsService.getAccountById(projectData.permissions[j].account, function(accDataErr, accountData){
+              if(!accDataErr){
+                $scope.collaborators.push(accountData.identities);
+              }else{
+                $scope.errors.push(accDataErr);
+              }
+            });
+          }
+         }else
+           {
+             $scope.errors.push(projDataErr);
+           }
+        });
+    };
+
 };
 
-   ProjectPermissionsModalController.$inject = ["$scope", "$modalInstance"];
+   ProjectPermissionsModalController.$inject = ["$scope", "$modalInstance", "projectsService", "AccountsService"];
    angSpa.controller("projectPermissionsModalController", ProjectPermissionsModalController);
 
 })();
