d77706ed49ae73388466125ba624582e10d60ac1
sharathm <codemod@gmail.com>
2014-02-28 16:00:34 +0530

    restyling console logs.

--- public/js/app/controllers/buildController.js
+++ public/js/app/controllers/buildController.js
@@ -1,17 +1,53 @@
 var BuildController = function($scope,$routeParams,BuildsService) {
- var collapseMap = {
-    "[install]": "[/install]",
-    "[script]":  "[/script]",
-    "[before]": "[/before]",
-    "[after_success]" : "[/after_success]",
-    "[before_install]" : "[/before_install]"
+  var collapseMap = {
+    "__SH_CMD__": "__SH_CMD_END__",
   };
 
 
 
   $scope.err = "";
-  
+
   var consoleLogs;
+
+
+  $scope.compressedLogs = [];
+  function compressConsoleLogs(data) {
+    var masterIndex = 0;
+    for(var i=0;i<data.length;i++) {
+      //Split the individual console element by newline
+      var splitData = data[i].output.split('\n');
+      //console.log('splits ' + splitData.length + " i: " + i);
+      var hasCmdTag = false;
+      var shouldCompress = false;
+      var consoleItem = getNewConsoleItem("","");
+      for(var j=0;j<splitData.length;j++) {
+        if(splitData[j].indexOf("__SH_CMD__")!== -1) {
+          hasCmdTag =true;
+          $scope.compressedLogs.push(consoleItem);
+          var cmds = splitData[j].split('|');
+          consoleItem = getNewConsoleItem(cmds[1],cmds[2]);
+          //consoleItem.output.push(cmds[2]);
+        }
+        else if(splitData[j].indexOf("__SH_CMD_END__") !== -1) {
+          shouldCompress = true;
+          //consoleItem.output.push(splitData[j]);
+          consoleItem.shouldCompress = true;
+          $scope.compressedLogs.push(consoleItem);
+          consoleItem = getNewConsoleItem("","");
+        }
+        else {
+          consoleItem.output.push(splitData[j]);
+
+        }
+
+      }
+      $scope.compressedLogs.push(consoleItem);
+      
+    }
+    console.log($scope.compressedLogs);
+  }
+
+
   $scope.init = function(){
     BuildsService.getConsoleByBuildNo($routeParams.projectId,$routeParams.buildNumber,function(err,data){
       if(err)
@@ -20,74 +56,30 @@ var BuildController = function($scope,$routeParams,BuildsService) {
         }
         else
           {
+
             console.log(data);
-            consoleLogs = data;
+            /*
+            //consoleLogs = data;
             //get the build status.. if its success, only then process it
             BuildsService.getById($routeParams.projectId,$routeParams.buildNumber,function(err,build) {
-              if(err) {
-                $scope.err = "Error getting build information";
-                processLogs(0);
-              }
-              else {
-                    processLogs(data,build.status);
+            if(err) {
+            $scope.err = "Error getting build information";
+            processLogs(0);
+            }
+            else {
+            processLogs(data,build.status);
 
-              }
-            });
+            }
+            }); */
+            $scope.consoleLogs = data;
+            compressConsoleLogs(data);
           }
 
     });
   };
   $scope.init();
-
-  $scope.newConsoleLogs = [];
-
-  function findPatternInString(patterns,str) {
-    //var patterns = _.values(c);
-    for(var i=0;i<patterns.length;i++) {
-      if(str.indexOf(patterns[i]) != -1) {
-        console.log('found pattern ' + patterns[i]);
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function getNewConsoleItem(title) {
-    return { output : [], shouldCompress : false, isShowing: false,title : title };
-
-  }
-  function processLogs(data,status) {
-    var createNewItem = false;
-    var consoleItem = getNewConsoleItem();
-    for(var i=0;i<data.length;i++) {
-
-      if( findPatternInString(_.keys(collapseMap),consoleLogs[i].output)) {
-        $scope.newConsoleLogs.push(consoleItem);
-        consoleItem = getNewConsoleItem(consoleLogs[i].output);
-
-        consoleItem.output.push(consoleLogs[i].output);
-        consoleItem.shouldCompress = (status === 1);
-
-      }
-      else if(findPatternInString(_.values(collapseMap),consoleLogs[i].output)) {
-        consoleItem.output.push(consoleLogs[i].output);
-        $scope.newConsoleLogs.push(consoleItem);
-        consoleItem = getNewConsoleItem("");
-
-      }
-
-      else {
-        consoleItem.output.push(consoleLogs[i].output);
-      }
-
-    }
-
-    $scope.newConsoleLogs.push(consoleItem);
-
-
-
-
-
+  function getNewConsoleItem(title,meta) {
+    return { output : [], shouldCompress : false, isShowing: false,title : title,meta:meta};
 
   }
 
