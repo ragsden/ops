798757ac64990a5a489cc44150dc6e9c35049f74
sharathm <codemod@gmail.com>
2014-03-03 14:18:52 +0530

    add console log colors

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config) {
             'public/js/lib/angular/angular.js',
             'public/js/bootstrap/js/ui-bootstrap-tpls-0.10.0.min.js',
             'public/js/lib/underscore-min.js',
+            'public/js/lib/ansi_up.js',
             'public/js/lib/angular/angular-*.js',
             'public/js/lib/angular/angular-mocks.js',
             'public/js/app/**/*.js',
