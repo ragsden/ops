9d2404ed6f6bd0ec6d1d549257e9d4e19ca6b9ec
sharathm <codemod@gmail.com>
2014-03-13 14:45:29 +0530

    run all unit tests in subdirectories

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,11 +20,11 @@ module.exports = function(grunt) {
     },
     exec : {
         test: {
-          command: "mkdir -p shippable/testresults && XUNIT_FILE=shippable/testresults/result.xml ./node_modules/mocha/bin/_mocha --timeout 5000 --reporter=xunit-file tests/node/**/*.js"
+          command: "mkdir -p shippable/testresults && XUNIT_FILE=shippable/testresults/result.xml ./node_modules/mocha/bin/_mocha --timeout 5000 --reporter=xunit-file --recursive tests/node"
 
         },
         coverage : {
-          command : "./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --timeout 5000 tests/node/**/*.js && ./node_modules/.bin/istanbul report cobertura --dir  shippable/codecoverage"
+          command : "./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --timeout 5000 --recursive tests/node && ./node_modules/.bin/istanbul report cobertura --dir  shippable/codecoverage"
         },
         clean : {
           command : "rm -Rf coverage shippable"
