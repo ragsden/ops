40fd62a048c47d89f2cddcfa5115724fcd30fff8
sharathm <codemod@gmail.com>
2014-03-18 21:08:32 +0530

    authorized access to analytics API

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,8 +1,12 @@
+function ensureAuthenticated(req,res,next) {
+	if(req.user) { next	(); }
+	else { res.send(401); }
+}
 module.exports.init = function(app) {
 
 	var accountAnalytics = require('./accountAnalytics')
 	, systemAnalytics = require('./systemAnalytics');
-	app.get('/analytics/accounts/new',accountAnalytics.getNewUsers);
-	app.get('/analytics/accounts/filter',accountAnalytics.filter);
-	app.get('/analytics/system',systemAnalytics.getTotalUsers,systemAnalytics.getLastRunTime,systemAnalytics.finished);
+	app.get('/analytics/accounts/new',ensureAuthenticated,accountAnalytics.getNewUsers);
+	app.get('/analytics/accounts/filter',ensureAuthenticated,accountAnalytics.filter);
+	app.get('/analytics/system',ensureAuthenticated,systemAnalytics.getTotalUsers,systemAnalytics.getLastRunTime,systemAnalytics.finished);
 };
