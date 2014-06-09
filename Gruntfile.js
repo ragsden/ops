a36c2d7039932859e53bbbe47ef37b81d34cc9ec
Raghu <raghu@shippable.com>
2014-06-08 20:04:17 -0700

    demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,8 +14,7 @@ module.exports = function(grunt) {
     karma: {
       unit: {
         configFile: 'karma.conf.js',
-        singleRun: true,
-        browsers: ['PhantomJS']
+        singleRun: true
       }
     },
     exec : {
